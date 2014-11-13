3f81616980884a7525cb96b333b7d66a0ee521ae
Mitch Tuck <matuck@matuck.com>
2014-11-12 23:24:25 -0500

    updates for the contact page.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,8 +3,42 @@
 /**
  * Module dependencies.
  */
+var nodemailer = require('nodemailer'),
+		config = require('../../config/config'),
+		async = require('async');
+
 exports.index = function(req, res) {
 	res.render('index', {
 		user: req.user || null
+	});
+};
+
+/**
+ * Send an email message
+ */
+exports.sendmessage = function(req, res, next) {
+	console.log(req.body);
+	async.waterfall([
+		// If valid email, send reset email using service
+		function(done) {
+			var smtpTransport = nodemailer.createTransport(config.mailer.options);
+			var mailOptions = {
+				to: 'suppoprt@elfreports.com',
+				from: req.body.email,
+				subject: 'Message from Website',
+				html: req.body.message
+			};
+			smtpTransport.sendMail(mailOptions, function(err) {
+				if (!err) {
+					res.send({
+						message: 'Your message has been sent.'
+					});
+				}
+
+				done(err);
+			});
+		}
+	], function(err) {
+		if (err) return next(err);
 	});
 };
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', '$location', 'Authentication',
-	function($scope, $location, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$location', '$http', 'Authentication',
+	function($scope, $location, $http, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
@@ -10,6 +10,21 @@ angular.module('core').controller('HomeController', ['$scope', '$location', 'Aut
 			if (user !== '' &&  user !== undefined) {
 				$location.path('/children');
 			}
+		};
+
+		$scope.sendmessage = function() {
+			$scope.success = $scope.error = null;
+
+			$http.post('/sendmessage', $scope.data).success(function(response) {
+				// Show user success message and clear form
+				$scope.message = null;
+				$scope.success = response.message;
+
+			}).error(function(response) {
+				// Show user error message and clear form
+				$scope.message = null;
+				$scope.error = response.message;
+			});
 		};
 	}
 ]);
